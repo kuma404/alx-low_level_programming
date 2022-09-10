000ed1d483bdd78bd2adcf4d591136b7478240f6
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-10 15:10:34 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * main - starting point
- *
- * Reaturn: Always 0
- */
+* main - starting point
+*
+* Reaturn: Always 0
+*/
 
 int main (void)
-{
+{		
 		puts("\"Programming is like building a multilingual puzzle");
-			     return (0);
+			return (0);
 }
