e44564215640ab19e7c0f57fa625cdce666fc7d3
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-10 16:30:32 -0700

    hei

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
 #!/bin/bash
+#include <sudio.h>
+/**
+ *main -Starting
+ *
+ *Return: prinf
+ */
+
+int main(void)
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return(0);
+}
