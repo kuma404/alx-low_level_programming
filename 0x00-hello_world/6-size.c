cee5d1adc0f831a6a183dbd6ba8c3bcc1f4eeacf
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-11 13:39:01 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	char hire = 0;
 	int dog = 0;
-	long int pig = 0:
-	long long int snake = 0;
+	long int pig = 0;
+	long long int snail = 0;
 	float cat = 0;
 
 		printf("Size of char: %ld byte(s)\n", sizeof(hire));
 			printf("Size of int: %ld byte(s)\n", sizeof(dog));
 				printf("Size of long int: %ld byte(s)\n", sizeof(pig));
-					printf("Size of long long int: %ld byte(s)\n", sizeof(snake));
+					printf("Size of long long int: %ld byte(s)\n", sizeof(snail));
 						printf("Size of float: %ld byte(s)\n", sizeof(cat));
 							return (0);
 }
