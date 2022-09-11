02ced28f81f6a12b67001b316c8a1d82d925570a
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-11 07:51:22 -0700

    fire

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,15 @@
 #!/bin/bash
+#include <stdio.h>
+/**
+*Main -produce the exact same output 
+*
+*Return: Alway 0 (Success)
+*/
+int main(void)
+{
+		printf("size of char: %1d byte(s)\n", sizeof(char));
+			printf("size of int: %1d byte(s)\n", sizeof(int));
+				printf("size of long int: %1d byte(s)\n", sizeof(long int));
+					printf("size of long long int: %1d byte(s)\n", sizeof(long long int));
+						printf("size of a float: %1d byte(s)\n", sizeof(float));
+							return (0)
