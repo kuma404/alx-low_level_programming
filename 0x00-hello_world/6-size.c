c273393ec15d9be185ff9abf3bedb44b47c615c2
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-11 07:53:41 -0700

    fire

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
 *Main -produce the exact same output 
@@ -12,4 +11,4 @@ int main(void)
 				printf("size of long int: %1d byte(s)\n", sizeof(long int));
 					printf("size of long long int: %1d byte(s)\n", sizeof(long long int));
 						printf("size of a float: %1d byte(s)\n", sizeof(float));
-							return (0)
+							return (0);
