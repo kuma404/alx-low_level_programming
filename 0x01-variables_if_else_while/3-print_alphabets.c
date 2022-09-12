3b055d4fd6fc502532eabb398051424e7355d584
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-12 16:03:37 -0700

    kill

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Prints the alphabet in lower and upper case
+ *
+ *Resume: Always (Sucess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
