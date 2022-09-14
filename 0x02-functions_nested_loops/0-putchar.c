7f36f33601e49602b97b3ab474ab1adb94a416f0
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-14 14:39:49 -0700

    no

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 _putchar('_');
-_putchar('p'); 
+_putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
@@ -16,7 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
-  return(0);
-
+return (0);
 }
