7038bee85833a4a9501bb1226f45e4065a94eee7
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-22 05:40:27 -0700

    kill

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_strncat -> function to append som charx
+*@dest: first param
+*@src: second param
+*@n: 3rd param
+*Return: string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n);
+	return (dest);
+}
