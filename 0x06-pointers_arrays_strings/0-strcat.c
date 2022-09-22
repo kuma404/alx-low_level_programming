17f6331b540bc14bf62cbc6345bfce6f02fdeba6
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-22 04:51:33 -0700

    like

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,9 +8,16 @@
 *
 *Return: pointer to @dest
 */
-
 char*_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int len = 0, i;
+	while (dest[len])
+		len++;
+	for (i = 0; scr[i] !=0; i++)
+	{
+		dest[len] = src[i];
+		len +=1;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
