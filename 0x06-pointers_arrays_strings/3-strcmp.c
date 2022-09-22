abc8869b2077a4ac42435d9a9ee4b8086b174412
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-22 06:07:53 -0700

    hell no

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strcmp -> for comparing purpose
- * @1: param1
- * @2: param2
- * Return: integer
- */
+* _strcmp -> for comparing purpose
+* @1: param1
+* @2: param2
+* Return: integer
+*/
 int _strcmp(char *s1, char *s2)
 {
 	int result;
