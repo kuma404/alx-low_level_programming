e7bff9750bd9002f4d296458e9571620417ac856
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-22 06:02:10 -0700

    hello

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp -> for comparing purpose
+ * @1: param1
+ * @2: param2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2);
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
