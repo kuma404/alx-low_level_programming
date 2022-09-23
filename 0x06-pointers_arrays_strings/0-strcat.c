042c5475314a0876f7df13f01e58261a6c4dc7fa
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-23 09:20:33 -0700

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,12 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int len = 0,i;
+	int len = 0, i;
+
 	while (dest[len])
 		len++;
-	for (1 = 0; src[i] !=0; i++)
+
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[len] = src[i];
 		len +=1;
