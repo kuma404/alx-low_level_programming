1e8e04e44aac85ca9dc3a17ebc9e080432c2d4d4
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-23 16:25:54 -0700

    hello

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,14 +6,41 @@
 *
 *Return: address of the encodeded string
 */
+
 char *leet(char *str)
 {
 	int i = 0;
 
-	while (str[i] !!= '\0')
+	while (str[i] != '\0')
 	{
 		str[i] = transform(str[i]);
 		i++;
 	}
 	return (str);
+}
+
+/**
+ * *
+ * transform - helper function to map letter with it's leet encoding
+ * @x; char to be encoded
+ *
+ * Return: the encoded char
+ */
+char transform(char x)
+{
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[8] = {'O' 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 8)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + 'e';
+			break;
+		}
+		i++;
+	}
+	return (replacement);
 }
