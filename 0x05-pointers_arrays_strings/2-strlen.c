22183d9f517a905b954abe7e02dacefe53bcbe2a
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-25 16:06:18 -0700

    high

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -> function to get the length of a string
+ *@S: sting function to be passed to this function
+ *Returns: length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
