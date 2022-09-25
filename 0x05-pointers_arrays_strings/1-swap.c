828bcc806e29515aa11f4b63364ec733b9738be2
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-25 15:19:00 -0700

    glory

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,9 @@
-#include "main.c"
+#include "main.h"
 /**
- *
+ *swap_int - swaps value of two integer string
+ *@a: an integer to swap
+ *@b: another integer to swap
+ *Return: void
  */
 void swap_int (int *a, int *b)
 {
