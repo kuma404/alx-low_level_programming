e061a5ed60972f04af967e8be583c3a4bb9a3abb
kuma404 <chikwendumiracle2020@gmail.com>
2022-09-25 17:21:05 -0700

    almost

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,19 @@
 #include "main.h"
-#include <string.h> 
+#include <string.h>
+
 /**
- *print _rev -> printing reverse string
- *@s: string to be printed in rev
- */
+*print _rev -> printing reverse string
+*@s: string to be printed in rev
+*/
+
 void print_rev(char *s)
 {
-		int i, n;
+	int i, n;
 
-		s = "hello"
-		n = strlen(s);
-		for (i = n-1; i >= 0; i--)
-		{
-			_putchar(s[i]);
-		}
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
