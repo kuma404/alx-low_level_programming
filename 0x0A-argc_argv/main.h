23b5e94f6b2d8fd30956f766add987a567bf0364
kuma404 <chikwendumiracle2020@gmail.com>
2022-10-09 14:29:27 -0700

    know

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+
+int _putchar(char c);                                                                                                                
+int check_num(char *str);
+
+int _putchar(char c);                                                                                                                
+int check_num(char *str);
